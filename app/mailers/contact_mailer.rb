db55ca113a7e31e946ef17efcf3cb8c2e776cd37
Vijay Brar <vijay.brar94@gmail.com>
2016-03-20 06:57:46 +0000

    Error resolved in contact_email 2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-    default to: "vijay.brar94@gmail.com"
+    default to: 'vijay.brar94@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
         
-        mail(from: email, subject: "Contact Form Message")
+        mail(from: email, subject: 'Contact Form Message')
     end
 end
